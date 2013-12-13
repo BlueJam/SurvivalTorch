ba97cba4cfc20b1d24da75b88a924422bdd04c20
BlueJam <bimrose@gmail.com>
2013-12-13 15:37:14 +0000

    Got the sos button working

--- SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchActivity.java
+++ SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchActivity.java
@@ -1,6 +1,7 @@
 package uk.co.bimrose.android.survivaltorch;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
@@ -9,6 +10,9 @@ public class TorchActivity extends SherlockFragmentActivity {
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    
+	    //stops screen closing, should be an option
+	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	    
 	    if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
 	      getSupportFragmentManager().beginTransaction()
--- SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchFragment.java
+++ SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchFragment.java
@@ -2,12 +2,14 @@ package uk.co.bimrose.android.survivaltorch;
 
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
@@ -17,7 +19,7 @@ public class TorchFragment extends SherlockFragment implements
 	Button buttonFull;
 	Button buttonSos;
 	Button buttonSosPreset;
-	
+
 	Camera cam = null;
 	Parameters p = null;
 	boolean isFlashOn = false;;
@@ -37,98 +39,124 @@ public class TorchFragment extends SherlockFragment implements
 
 		return (result);
 	}
-	
+
 	@Override
 	public void onPause() {
-	    super.onPause();
-	    turnOffFlash();
+		super.onPause();
+		turnOffFlash();
 	}
-	
+
 	@Override
 	public void onResume() {
-	    super.onResume();
-	    if(isFlashOn)
-	        turnOnFlash();
+		super.onResume();
+		if (isFlashOn)
+			turnOnFlash();
 	}
-	
+
 	@Override
 	public void onStart() {
-	    super.onStart();
-	    getCamera();
+		super.onStart();
+		getCamera();
 	}
-	
+
 	@Override
 	public void onStop() {
-	    super.onStop();
-	    if (cam != null) {
-	    	cam.release();
-	    	cam = null;
-	    }
+		super.onStop();
+		if (cam != null) {
+			cam.release();
+			cam = null;
+		}
 	}
 
 	@Override
 	public void onClick(View view) {
-		
+
 		switch (view.getId()) {
 		case R.id.button_full:
-			if(!isFlashOn){
+			if (!isFlashOn) {
 				turnOnFlash();
-			}else{
+			} else {
 				turnOffFlash();
 			}
-			
 			break;
 		case R.id.button_sos:
-			//turnOffFlash();
+			new Sos().execute();
 			break;
 		case R.id.button_sos_preset:
-			// handle button B click;
+			// sosPreset
 			break;
 		default:
 			throw new RuntimeException("Unknow button ID");
 		}
 
 	}
-	
-	//get camera parameters
+
+	// get camera parameters
 	private void getCamera() {
-	    if (cam == null) {
-	        try {
-	        	cam = Camera.open();
-	            p = cam.getParameters();
-	        } catch (RuntimeException e) {
-	            Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
-	        }
-	    }
-	}
-	
-	//Turn on flash
-	private void turnOnFlash() {
-	    if (!isFlashOn) {
-	        if (cam == null || p == null) {
-	            return;
-	        }
-	        buttonFull.setText("test2");
-	        p = cam.getParameters();
-	        p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-	        cam.setParameters(p);
-	        cam.startPreview();
-	        isFlashOn = true;
-	    }
-	 
-	}
-	
-	private void turnOffFlash() {
-	    if (isFlashOn) {
-	        if (cam == null || p == null) {
-	            return;
-	        }
-	        p = cam.getParameters();
-	        p.setFlashMode(Parameters.FLASH_MODE_OFF);
-	        cam.setParameters(p);
-	        cam.stopPreview();
-	        isFlashOn = false;
-	    }
+		if (cam == null) {
+			try {
+				cam = Camera.open();
+				p = cam.getParameters();
+			} catch (RuntimeException e) {
+				Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
+			}
+		}
 	}
 
+	// Turn on flash
+	private void turnOnFlash() {
+		if (!isFlashOn) {
+			if (cam == null || p == null) {
+				return;
+			}
+			p = cam.getParameters();
+			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			cam.setParameters(p);
+			cam.startPreview();
+			isFlashOn = true;
+		}
+
+	}
+
+	private void turnOffFlash() {
+		if (isFlashOn) {
+			if (cam == null || p == null) {
+				return;
+			}
+			p = cam.getParameters();
+			p.setFlashMode(Parameters.FLASH_MODE_OFF);
+			cam.setParameters(p);
+			cam.stopPreview();
+			isFlashOn = false;
+		}
+	}
+
+	private class Sos extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... args) {
+
+			if (isFlashOn) {
+				turnOffFlash();
+			}
+
+			try {
+				//dot dot dot, dash dash dash, dot dot dot
+				for (int i = 0; i < 9; i++) {
+					int flashOn = 500;
+					int sleepTime = 500;
+					if (i > 2 && i < 6) {
+						flashOn = 1500;
+					}
+					turnOnFlash();
+					Thread.sleep(flashOn);
+					turnOffFlash();
+					Thread.sleep(sleepTime);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+	}
 }
