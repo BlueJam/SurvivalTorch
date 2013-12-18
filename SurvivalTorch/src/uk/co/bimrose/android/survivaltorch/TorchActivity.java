4f814275179759fa7b0850c5431cd277bac91662
BlueJam <bimrose@gmail.com>
2013-12-18 14:49:27 +0000

    just getting the light sensor up and running.

--- SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchActivity.java
+++ SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchActivity.java
@@ -1,18 +1,38 @@
 package uk.co.bimrose.android.survivaltorch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.WindowManager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
 public class TorchActivity extends SherlockFragmentActivity {
+	
+	boolean keepScreenOn = false;
 	
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    
-	    //stops screen closing, should be an option
-	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	    
+	    //have they selected to keep the screen on?
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		keepScreenOn = Boolean.valueOf(prefs.getBoolean("keepscreenon", false));
+		if(keepScreenOn){
+			//stops screen closing, should be an option
+		    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		}
+		
+		//check for light sensor on the device
+		sensorCheck();
 	    
 	    if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
 	      getSupportFragmentManager().beginTransaction()
@@ -20,4 +40,33 @@ public class TorchActivity extends SherlockFragmentActivity {
 	                                      new TorchFragment()).commit();
 	    }
 	  }
+	  
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			new MenuInflater(this).inflate(R.menu.options, menu);
+			return (super.onCreateOptionsMenu(menu));
+		}
+	  
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.prefs:
+				TorchFragment.stop = true;
+				startActivity(new Intent(this, Preferences.class));
+				return (true);
+			}
+			return (super.onOptionsItemSelected(item));
+		}
+	  
+		private void sensorCheck() {
+			SensorManager mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+			if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
+				// Yep, light sensor present
+				LightSensor.isThereALightSensor = true;
+			} else {
+				// No light sensor
+				LightSensor.isThereALightSensor = false;
+			}
+		}
+	  
 	}
--- SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchFragment.java
+++ SurvivalTorch/src/uk/co/bimrose/android/survivaltorch/TorchFragment.java
@@ -1,9 +1,11 @@
 package uk.co.bimrose.android.survivaltorch;
 
+import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,10 +21,24 @@ public class TorchFragment extends SherlockFragment implements
 	Button buttonFull;
 	Button buttonSos;
 	Button buttonSosPreset;
+	Button buttonStop;
+	TextView message;
 
 	Camera cam = null;
 	Parameters p = null;
-	boolean isFlashOn = false;;
+	boolean isFlashOn = false;
+	boolean loopForever = false;
+	boolean single = true;
+	boolean loopUntilLight = false;
+	int timeBetweenSignals = 5;
+	boolean stopOnLowBattery = true;
+	boolean keepScreenOn = false;
+	int list = 50;
+	int loopXTimes = 1;
+	public static boolean stop = false;
+	int sosSpeed = 500;
+	
+	public static float lightLevel;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
@@ -32,10 +48,15 @@ public class TorchFragment extends SherlockFragment implements
 		buttonFull = (Button) result.findViewById(R.id.button_full);
 		buttonSos = (Button) result.findViewById(R.id.button_sos);
 		buttonSosPreset = (Button) result.findViewById(R.id.button_sos_preset);
+		buttonStop = (Button) result.findViewById(R.id.button_stop);
+		message = (TextView) result.findViewById(R.id.message);
 
 		buttonFull.setOnClickListener(this);
 		buttonSos.setOnClickListener(this);
 		buttonSosPreset.setOnClickListener(this);
+		buttonStop.setOnClickListener(this);
+
+		// getSherlockActivity().getSupportActionBar().setHomeButtonEnabled(true);
 
 		return (result);
 	}
@@ -49,8 +70,43 @@ public class TorchFragment extends SherlockFragment implements
 	@Override
 	public void onResume() {
 		super.onResume();
-		if (isFlashOn)
+		
+		if (isFlashOn) {
 			turnOnFlash();
+		}
+
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getActivity());
+
+		loopForever = Boolean.valueOf(prefs.getBoolean("loopforever", false));
+		loopXTimes = Integer.valueOf(prefs.getString("loopxtimes", "1"));
+		timeBetweenSignals = Integer.valueOf(prefs.getString(
+				"timebetweenloops", "5"));
+		sosSpeed = Integer.valueOf(prefs.getString("sosspeed", "500"));
+		
+		message.setText("");
+		loopUntilLight = Boolean.valueOf(prefs.getBoolean("loopuntillight",
+				false));
+		//If the light sensor has been checked then we need to check if we have one
+		//If there isn't one, we reset the value and send the user a message
+		if(loopUntilLight){
+			// if there is no light sensor then clear the preference and post a message
+			if (!LightSensor.isThereALightSensor) {
+				prefs.edit().remove("loopuntillight").commit();
+				message.setText("Sorry, you don't have a light sensor");
+			}else{
+				LightSensor lS = new LightSensor();
+				message.setText(Float.toString(lightLevel));
+			}
+
+		}
+		
+		stopOnLowBattery = Boolean.valueOf(prefs.getBoolean("stoponlowbattery",
+				true));
+		list = Integer.valueOf(prefs.getString("list", "5"));
+
+		keepScreenOn = Boolean.valueOf(prefs.getBoolean("keepscreenon", false));
+
 	}
 
 	@Override
@@ -71,6 +127,8 @@ public class TorchFragment extends SherlockFragment implements
 	@Override
 	public void onClick(View view) {
 
+		stop = false;
+
 		switch (view.getId()) {
 		case R.id.button_full:
 			if (!isFlashOn) {
@@ -80,10 +138,16 @@ public class TorchFragment extends SherlockFragment implements
 			}
 			break;
 		case R.id.button_sos:
+			single = true;
 			new Sos().execute();
 			break;
 		case R.id.button_sos_preset:
-			// sosPreset
+			single = false;
+			new Sos().execute();
+			break;
+		case R.id.button_stop:
+			turnOffFlash();
+			stop = true;
 			break;
 		default:
 			throw new RuntimeException("Unknow button ID");
@@ -139,24 +203,50 @@ public class TorchFragment extends SherlockFragment implements
 				turnOffFlash();
 			}
 
+			if (single) {
+				sos();
+			} else {
+				if (loopForever) {
+					while (!stop) {
+						sos();
+					}
+				} else if (loopXTimes > 0) {
+					for (int i = 0; i < loopXTimes; i++) {
+						sos();
+						if (stop) {
+							break;
+						}
+					}
+				}
+
+			}
+
+			return null;
+		}
+
+		private void sos() {
 			try {
-				//dot dot dot, dash dash dash, dot dot dot
+				// dot dot dot, dash dash dash, dot dot dot
 				for (int i = 0; i < 9; i++) {
-					int flashOn = 500;
-					int sleepTime = 500;
+					int flashOn = sosSpeed;
+					int sleepTime = sosSpeed;
 					if (i > 2 && i < 6) {
-						flashOn = 1500;
+						flashOn = sosSpeed * 3;
 					}
 					turnOnFlash();
 					Thread.sleep(flashOn);
 					turnOffFlash();
 					Thread.sleep(sleepTime);
+					if (stop) {
+						break;
+					}
 				}
+				Thread.sleep(timeBetweenSignals * 1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
-			return null;
 		}
+
 	}
+
 }
